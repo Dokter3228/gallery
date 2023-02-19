077c56489e155cdc8636dc2c45a7943b4e8bc9f4
Dmitrii <Dokter322@yandex.ru>
2023-02-19 15:34:51 +0300

    add basic Redux Toolkit setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './components/App/Main';
+import Main from './App/Main';
+import { store } from './App/store'
+import { Provider } from 'react-redux'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
+      <Provider store={store}>
           <Main />
+      </Provider>
   </React.StrictMode>
 );
