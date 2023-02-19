58d959901d12bc4cfa1f1fe4a57c9ee58863fe2e
Dmitrii <Dokter322@yandex.ru>
2023-02-19 15:05:38 +0300

    refactor everything into typescript

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Main from './components/App/Main';
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
           <Main />
